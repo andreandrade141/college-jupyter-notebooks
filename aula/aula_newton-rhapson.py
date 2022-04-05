7a6be71b6764cb9d1d2f1bd672b042e623c1a0e8
AndreAndrade141 <andrelsandrade43@gmail.com>
2022-04-05 20:23:21 -0300

    Update on class examples

--- aula/aula_newton-rhapson.py
+++ aula/aula_newton-rhapson.py
@@ -1,5 +1,6 @@
 from cmath import e, pi
 import sympy as sp
+# from IPython.display import display
 
 # Exercício 1:
 
@@ -18,7 +19,28 @@ print(x3abs)
 # Exercício 2:
 
 xaprox = 2.72
-x = e
+xc = e
 
-error = abs((x-xaprox)/x)
+error = abs((xc-xaprox)/xc)
 print("error: ", error)
+
+# Exercício 3:
+
+x = sp.symbols('x')
+f = x**3 - sp.sqrt(x) + sp.sin(x) + sp.exp(x) + sp.log(x) - 4
+
+print("f(x) = ")
+print(f)
+# display(f, raw=True)
+
+# Exercício 4:
+a = 4
+print("EQUACAO PARA X = 4")
+# Não funciona no vscode sem configurar o terminal padrão para o Ipython
+# display(f.subs(x, a))
+print(f.subs(x, a))
+
+solvefor4 = f.subs(x, a).evalf()
+print("RESOLVE F(4)")
+# display(solvefor4)
+print(solvefor4)
