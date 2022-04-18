0ce5ef1f8a03a23d740888d3476b6f6188bf5532
AndreAndrade141 <andrelsandrade43@gmail.com>
2022-04-17 22:21:05 -0300

    MA1-PARTE 1

--- exercícios/arquivo.py
+++ exercícios/arquivo.py
@@ -1,29 +1,65 @@
+"""
+    ANDRÉ LUÍS SEBASTIÃO ANDRADE - 104004
+"""
 import numpy as np
 
-matriz = []
-v_constante = []
 
-def solucao():
-    res = np.linalg.solve(matriz, v_constante)
-    return res
+def tlinha(A, i, j):
+    buffer = A[i]
+    A[i] = A[j]
+    A[j] = buffer
+    return A
 
-def triangular_superior():
-    pass
 
-def solucao_triangular_superior():
-    pass
+def mlinha(A, i, alfa):
+    A[i] = alfa*A[i]
+    return A
 
-def main():
-    solve = solucao()
-    superior = triangular_superior()
-    tri_solve = solucao_triangular_superior()
 
-    print("RESOLUÇÃO DA MATRIZ A PELO VETOR CONSTANTE B:")
-    print("Matriz A:", matriz)
-    print("Vetor B:", v_constante)
-    print("-----------------------------")
-    print("Resolucao: ", solve)
-    print("Triangular: ", superior)
-    print("Resolucao Triangular: ", tri_solve)
+def slinha(A, i, k, alfa):
+    A[i] = A[i] + alfa*A[k]
+    return A
 
-main()
+
+def step_gauss(A, k):
+    L = range(len(A))
+    for j in L[k+1:]:
+        m = -A[j, k]/A[k, k]
+        A = slinha(A, j, k, m)
+    return A
+
+
+def triangular(A, B):
+    L = len(A)
+    for k in range(L):
+        if A[k, k] != 0:
+            A = step_gauss(A, k)
+            print(A)
+        else:
+            print(' falhou no passo '), k
+    solve_triangular(A, B)
+    return A
+
+
+def solve_triangular(A, B):
+    x3 = B[2, 0]/A[2, 2]
+
+    def x2():
+        x2 = (B[1, 0]-x3)/A[1, 1]
+        return x2
+
+    def x1():
+        x1 = (B[0, 0]-x2()-x3)/A[0, 0]
+        return x1
+
+    print(f'SOLUCOES: x1:{x1()}, x2:{x2()}, x3{x3}')
+
+
+a = np.array([[2, 1, 3],
+              [-1, -3, 2],
+              [3, 1, -3]])
+b = np.array([[-1],
+              [12],
+              [0], ])
+
+B = triangular(a, b)
